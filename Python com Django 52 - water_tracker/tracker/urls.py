4e965e94acdf88bbed2a48feab75e9a8e8534aec
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2025-03-07 13:56:09 -0300

    urls.py

--- Python com Django 52 - water_tracker/tracker/urls.py
+++ Python com Django 52 - water_tracker/tracker/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('add/', views.add_water_consumption, name='add_water_consumption'),
     path('chart/', views.water_consumption_chart, name='water_consumption_chart'),
+    path('list/', views.water_consumption_list, name='water_consumption_list'),
+    path('edit/<int:pk>/', views.edit_water_consumption, name='edit_water_consumption'),
+    path('delete/<int:pk>/', views.delete_water_consumption, name='delete_water_consumption'),
 ]
